47e6c972ee9d452c3dea627cd27f2693ff22b092
Ibsaa J. Adam <ibsaa.adam1@gmail.com>
2022-06-21 15:03:24 -0400

    changed bubbles

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,23 +6,9 @@ import './assets/BubbleStyle.css';
 
 const Home = () => {
   return (
-    <div name='home' 
-         className="w-full h-screen bg-[#0a192f]" 
-
-        >
+    <div name='home' className="w-full h-screen bg-[#0a192f]" >
       {/* Container */}
       <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
-        {/* Bubbles */}
-        <div className='flex content-center justify-around bubbles'>
-          <img className="bubble w-[50px] h-[50px] animate-fall overflow-hidden" src={Bubble} alt="bubble" />
-          <img className="bubble w-[50px] h-[50px] animate-fall overflow-hidden" src={Bubble} alt="bubble" />
-          <img className="bubble w-[50px] h-[50px] animate-fall overflow-hidden" src={Bubble} alt="bubble" />
-          <img className="bubble w-[50px] h-[50px] animate-fall overflow-hidden" src={Bubble} alt="bubble" />
-          <img className="bubble w-[50px] h-[50px] animate-fall overflow-hidden" src={Bubble} alt="bubble" />
-          <img className="bubble w-[50px] h-[50px] animate-fall overflow-hidden" src={Bubble} alt="bubble" />
-          <img className="bubble w-[50px] h-[50px] animate-fall overflow-hidden" src={Bubble} alt="bubble" />
-        </div>
-        {/* End of Bubbles */}
         <p className='text-blue-300'>Hi, my name is</p>
         <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>
           Ibsaa Adam
@@ -44,6 +30,24 @@ const Home = () => {
           </Link>
         </div>
       </div>
+        {/* Bubbles */}
+        <div className='bubbles flex align-center justify-around relative bottom-[-100px]'>
+          <img className="bubble w-[50px] h-[50px] animate-fall overflow-hidden" src={Bubble} alt="bubble" />
+          <img className="bubble w-[50px] h-[50px] animate-fall overflow-hidden" src={Bubble} alt="bubble" />
+          <img className="bubble w-[50px] h-[50px] animate-fall overflow-hidden" src={Bubble} alt="bubble" />
+          <img className="bubble w-[50px] h-[50px] animate-fall overflow-hidden" src={Bubble} alt="bubble" />
+          <img className="bubble w-[50px] h-[50px] animate-fall overflow-hidden" src={Bubble} alt="bubble" />
+          <img className="bubble w-[50px] h-[50px] animate-fall overflow-hidden" src={Bubble} alt="bubble" />
+          <img className="bubble w-[50px] h-[50px] animate-fall overflow-hidden" src={Bubble} alt="bubble" />
+          <img className="bubble w-[50px] h-[50px] animate-fall overflow-hidden" src={Bubble} alt="bubble" />
+          <img className="bubble w-[50px] h-[50px] animate-fall overflow-hidden" src={Bubble} alt="bubble" />
+          <img className="bubble w-[50px] h-[50px] animate-fall overflow-hidden" src={Bubble} alt="bubble" />
+          <img className="bubble w-[50px] h-[50px] animate-fall overflow-hidden" src={Bubble} alt="bubble" />
+          <img className="bubble w-[50px] h-[50px] animate-fall overflow-hidden" src={Bubble} alt="bubble" />
+          <img className="bubble w-[50px] h-[50px] animate-fall overflow-hidden" src={Bubble} alt="bubble" />
+          <img className="bubble w-[50px] h-[50px] animate-fall overflow-hidden" src={Bubble} alt="bubble" />
+        </div>
+        {/* End of Bubbles */}
     </div>
   );
 };
