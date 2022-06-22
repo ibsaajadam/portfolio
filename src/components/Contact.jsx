c697d61934609b7fc9b3bb8d086823e463166088
Ibsaa J. Adam <ibsaa.adam1@gmail.com>
2022-06-22 00:57:54 -0400

    changed responsiveness for iphone 5

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,7 +6,7 @@ const Contact = () => {
         <form method='POST' action="https://getform.io/f/c57ce9a5-89a4-439a-9bf9-ebbe519af077" className='flex flex-col max-w-[600px] w-full'>
             <div className='pb-8'>
                 <p className='text-4xl font-bold inline border-b-4 border-blue-300 text-gray-300'>Contact</p>
-                <p className='text-gray-300 py-4'>Submit the form below or send me an email - ibsaa.adam1@gmail.com</p>
+                <p className='text-gray-300 py-4'>Feel free to message me below or send me an email at: ibsaa.adam1@gmail.com</p>
             </div>
             <input className='bg-[#ccd6f6] p-2' type="text" placeholder='Name' name='name' />
             <input className='my-4 p-2 bg-[#ccd6f6]' type="email" placeholder='Email' name='email' />
