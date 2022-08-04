2bdb51fd1f87f6efe002ed8ed9305b442852d4d8
Ibsaa J. Adam <ibsaa.adam1@gmail.com>
2022-08-04 16:44:36 -0400

    updated readme

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -36,7 +36,7 @@ const Work = () => {
             {/* Hover Effects */}
             <div className='opacity-0 group-hover:opacity-100'>
               <span className='text-2xl font-bold text-white tracking-wider'>
-                JavaScript Login
+                WNBA Jobs
               </span>
               <div className='pt-8 text-center'>
                 <a href='https://javascript-front-end-login.netlify.app/' target="_blank" rel="noreferrer">
