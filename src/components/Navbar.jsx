393bd40a3a8467b9648996eef43cecb7a6166256
Ibsaa J. Adam <ibsaa.adam1@gmail.com>
2024-02-21 22:11:33 -0500

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -127,7 +127,7 @@ const Navbar = () => {
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]'>
             <a
               className='flex justify-between items-center w-full text-gray-300'
-              href='https://drive.google.com/file/d/19UIsg1rZmIq6uBygGbwEzR3AxRfMgj_L/view?usp=sharing'
+              href='https://drive.google.com/file/d/106viVh2XBwfJkGxm1my-OIqaiVUdqcvX/view?usp=sharing'
               target="_blank"
               rel="noreferrer"
             >
