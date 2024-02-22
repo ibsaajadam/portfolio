e25eb0ed24c4927fee34af7037d8b72b9b8c4e57
Ibsaa J. Adam <ibsaa.adam1@gmail.com>
2024-02-22 01:08:49 -0500

    Update Work.jsx

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -78,31 +78,6 @@ const Work = () => {
           </div>
 
 
-          {/* <div
-            style={{ backgroundImage: `url(${Map})` }}
-            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
-          >
-            <div className='opacity-0 group-hover:opacity-100'>
-              <span className='text-2xl font-bold text-white tracking-wider'>
-                React JS Google Map
-              </span>
-              <div className='pt-8 text-center'>
-                <a href='https://boisterous-sundae-6dbd4d.netlify.app/' target="_blank" rel="noreferrer">
-                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
-                    Demo
-                  </button>
-                </a>
-                <a href='https://github.com/ibsaajadam/MapsAPI-React' target="_blank" rel="noreferrer">
-                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
-                    Code
-                  </button>
-                </a>
-              </div>
-            </div>
-           </div> */}
-
-
-
           {/* Grid Item */}
           <div
             style={{ backgroundImage: `url(${Five})` }}
@@ -276,6 +251,30 @@ const Work = () => {
             </div>
           </div>
         </div>
+        
+
+        <div
+            style={{ backgroundImage: `url(${Map})` }}
+            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
+          >
+            <div className='opacity-0 group-hover:opacity-100'>
+              <span className='text-2xl font-bold text-white tracking-wider'>
+                React JS Google Map
+              </span>
+              <div className='pt-8 text-center'>
+                <a href='https://boisterous-sundae-6dbd4d.netlify.app/' target="_blank" rel="noreferrer">
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Demo
+                  </button>
+                </a>
+                <a href='https://github.com/ibsaajadam/MapsAPI-React' target="_blank" rel="noreferrer">
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+           </div>
           
         </div>
       </div>
