f829ad0d193113540c014227a0f9fb4ea6b4e0c7
Dana Jackson <danajackson@Danas-MacBook-Air.local>
2024-04-11 20:49:51 -0400

    comment out resume

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -125,14 +125,14 @@ const Navbar = () => {
             </a>
           </li>
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]'>
-            <a
+            {/* <a
               className='flex justify-between items-center w-full text-gray-300'
               href='https://drive.google.com/file/d/1fX08gW6eTfVxIGLgq0di6jZvs1Jo979g/view?usp=sharing'
               target="_blank"
               rel="noreferrer"
             >
               Resume <BsFillPersonLinesFill size={30} />
-            </a>
+            </a> */}
           </li>
         </ul>
       </div>
